177a1b9f729dabc94e97afd4bd1365f3d4ac7fec
Robin Marchart <robin.marchart@gmail.com>
2024-08-12 16:14:37 +0200

    revert back

--- module.nix
+++ module.nix
@@ -11,10 +11,9 @@ in
 {
   options.df-manager = lib.mkOption {
     type =
-      with lib.types;
-      listOf (submodule {
+      lib.types.listOf (lib.types.submodule {
         dwarf-fortress = lib.mkOption {
-          type = package;
+          type = lib.types.package;
           default = pkgs.dwarf-fortress;
           example = pkgs.dwarf-fortress-packages.dwarf-fortress_0_47_05;
           description = "dwarf fortress package (with passthrough dwarf-fortress, dfhack, twbt)";
@@ -26,7 +25,7 @@ in
         enableFPS = lib.mkEnableOption "enable showing fps";
         enableSound = lib.mkEnableOption "enable playing sound";
         dfSettings = lib.mkOption {
-          type = attrsOf (attrsOf str);
+          type = lib.types.attrsOf (lib.types.attrsOf lib.types.str);
           default = { };
           example = {
             d_init = {
@@ -37,23 +36,23 @@ in
           description = "additional settings";
         };
         saveLocation = lib.mkOption {
-          type = str;
+          type = lib.types.str;
           default = "${config.home.homeDirectory}/dwarf-fortress/default-saves";
           description = "where game saves should be kept";
         };
         modsList = lib.mkOption {
-          type = listOf package;
+          type = lib.types.listOf lib.types.package;
           default = [ ];
           example = with pkgs.dwarf-fortress-packages.mods; [ vettlingr ];
           description = "mods to install. derivation gets overlayed onto dwarf fortress folder.";
         };
         extraPackages = lib.mkOption {
-          type = listOf package;
+          type = lib.types.listOf lib.types.package;
           default = [ ];
           description = "extra derivations that can overwrite anything but settings";
         };
         suffix = lib.mkOption {
-          type = str;
+          type = lib.types.str;
           default = "";
           description = "suffix for launcher script. prevents conflicts with multiple concurrent installs";
         };
@@ -61,7 +60,7 @@ in
     default = [ ];
     description = "dwarf fortress installs that should be prepared";
   };
-  config.home.packages = [pkgs.hello] ++ map (
+  config.home.packages =  map (
     manager:
     pkgs.dwarf-fortress-packages.df-manager.override {
       inherit (manager)
